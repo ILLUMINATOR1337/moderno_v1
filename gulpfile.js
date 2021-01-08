77213b8f614daa19c37f12246809c686d5947a3f
ILLUMINATOR1337 <danil_zagirov@mail.ru>
2021-01-08 23:17:32 +0500

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -52,8 +52,8 @@ function scripts() {
 
 function styles() {
    return src([
-        'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/normalize.css/normolize.css',
+        'node_modules/slick-carousel/slick/slick.css',
+        'node_modules/normalize.css/normalize.css',
         'app/scss/style.scss'
       ])
       .pipe(scss({outputStyle: 'compressed'}))
